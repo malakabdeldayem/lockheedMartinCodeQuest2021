f8b3b9a56294f8badc5dfc0e21a657585301ea58
Malak Abdeldayem <64870300+malakabdeldayem@users.noreply.github.com>
2021-04-24 15:41:57 -0400

    Add files via upload

---
+++ biggerIsBetter.py
@@ -0,0 +1,19 @@
+no1= input()
+no2 = input()
+no3 = input()
+
+no1 = no1.split()
+no2 = no2.split()
+no3 = no3.split()
+
+max1 = max(no1)
+max2 = max(no2)
+max3 = max(no3)
+
+print(max1)
+print(max2)
+print(max3)
+
+#1 2 3 4
+#30 15 20 10
+#55 10 45 60 15 45 25 30 
---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import math
+import string 
+cases = int(sys.stdin.readline().rstrip())
+for caseNum in range(cases): 
+  print(sys.stdin.readline().rstrip())
+
+  num = int(sys.stdin.readline().rstrip())
+  
+  max = max(num)
+
+  print(max)
+
+
+#1 2 3 4
+#30 15 20 10
+#55 10 45 60 15 45 25 30 
+
